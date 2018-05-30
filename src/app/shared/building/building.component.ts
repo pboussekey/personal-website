82e37e68e8a2e52ef12706002b1d50f877fcc387
pboussekey <pboussekey@thestudnet.com>
2018-05-30 13:26:17 +0200

    Optimize building style

--- src/app/shared/building/building.component.ts
+++ src/app/shared/building/building.component.ts
@@ -1,7 +1,7 @@
-import { 
-    Component, 
-    HostBinding, 
-    Input, 
+import {
+    Component,
+    HostBinding,
+    Input,
     OnInit
 } from '@angular/core';
 
@@ -16,16 +16,20 @@ export class BuildingComponent implements OnInit {
     @HostBinding("style.height.px") height : number;
     @Input() delay : number;
     @Input() duration : number;
-    public windows : number[] = [];
+    public windows : any[] = [];
     public doorPlace : number;
-    
+
     ngOnInit() {
         this.windows = [];
-        this.width = Math.floor(Math.random() * 4) * 50 + 200;
-        this.height = Math.floor(Math.random() * 8) * 50 + 200;
-        var windowLength = (this.width) / 50 * (this.height - 100) / 50;
+        this.width = Math.floor(Math.random() * 6) * 50 + 400;
+        this.height = Math.floor(Math.random() * 14) * 50 + 400;
+        var windowWidth = (this.width) / 50;
+        var windowLength = windowWidth * (this.height - 150) / 50;
         for(var i = 0; i < windowLength; i++){
-          this.windows.push(Math.floor(Math.random() * 400));
+          var rand = Math.random() * 400;
+          if(rand < 100){
+            this.windows.push({ x :  Math.floor(i % windowWidth) * 50, y : Math.floor(i / windowWidth) * 50, value : rand });
+          }
         }
         this.doorPlace = Math.random() > 0.5 ? Math.max(10,Math.floor(Math.random() * (this.width / 50)) * 50 - 38) : 0;
         this.width+=12;
